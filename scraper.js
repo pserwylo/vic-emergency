2943a1aea58d1b53d197f1293a80492e45942479
Peter Serwylo <peter@serwylo.com>
2024-12-30 12:29:09 +1100

    Insert geojson and hash, only insert new record if changed.

--- scraper.js
+++ scraper.js
@@ -1,29 +1,32 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
-
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+const request = require("request");
+const sqlite3 = require("sqlite3").verbose();
+const { createHash } = require('crypto');
 
 function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE impacted_area (\n" +
+			"    geojson text                               not null,\n" +
+			"    geojson_sha256 text                        not null,\n" +
+			"    date    datetime default CURRENT_TIMESTAMP not null\n" +
+			"    constraint impacted_area_hash unique on conflict ignore\n" +
+			")");
 		callback(db);
 	});
 }
 
-function updateRow(db, value) {
+function updateRow(db, geojson, geojsonHash) {
 	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
+	var statement = db.prepare("INSERT INTO impacted_area (geojson, geojson_sha256) VALUES (?, ?)");
+	statement.run(geojson, geojsonHash);
 	statement.finalize();
 }
 
 function readRows(db) {
 	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
+	db.each("SELECT rowid AS id, geojson, geojson_sha256 FROM impacted_area", function(err, row) {
+		console.log(row.geojson_sha256);
 	});
 }
 
@@ -40,18 +43,10 @@ function fetchPage(url, callback) {
 }
 
 function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
+	fetchPage("https://emergency.vic.gov.au/public/impact-areas-geojson.json", function (geojson) {
+		const hash = createHash('sha256').update(geojson).digest('hex');
+		updateRow(db, geojson, hash);
 		readRows(db);
-
 		db.close();
 	});
 }
